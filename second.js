b2b9e40456362f9570e687f4777917db7f3cb26d
devwith-rprasit29 <pkcprasit@gmail.com>
2026-08-02 01:37:06 +0530

    second code in js

---
+++ second.js
@@ -0,0 +1,11 @@
+//create a variable and assign a value to it
+//print of Hello Prasit Using variable
+
+var person ="Prasit"
+var greetings ="Hello"
+//print of hello only
+console.log(greetings)
+//print of name only
+console.log(person)
+// together 
+console.log(greetings, person)
