6ddd3adf04cdda86ad03db5e0a5cd3c35cc9d287
devwith-rprasit29 <pkcprasit@gmail.com>
2026-08-04 23:24:50 +0530

    Problem2_besic

---
+++ prob2.js
@@ -0,0 +1,10 @@
+//find the square of a number 
+let a = 2;
+let square = a*a;
+console.log("Square of two Number is :" , square)
+
+//Find qube of a number 
+let b = 2;
+let qube = b*b*b;
+console.log("Qube of two Number is :" , qube)
+
