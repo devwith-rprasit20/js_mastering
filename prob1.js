70f4dd1e35becd322d98a3bd0eaccb2d2e146b46
devwith-rprasit29 <pkcprasit@gmail.com>
2026-08-04 23:15:17 +0530

    Problem1_numericals_besic

---
+++ prob1.js
@@ -0,0 +1,18 @@
+/*Create two variables: num1 = 25, num2 = 10.
+Print:
+Sum
+Difference
+Product
+Quotient
+Remainder
+*/
+
+let num1 = 25;
+let num2 = 10;
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1%num2)
+
